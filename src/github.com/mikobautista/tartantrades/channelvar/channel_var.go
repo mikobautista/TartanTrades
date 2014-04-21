42b2fa1cadd2802ad538ab1822954dc5eca75a29
Mat Gray <me@mgray.me>
2014-04-21 00:31:15 -0400

    resolver now listens for tcp connection from trade server, generates hash and responds

---
+++ src/github.com/mikobautista/tartantrades/channelvar/channel_var.go
@@ -0,0 +1,89 @@
+package channelvar
+
+import (
+	"github.com/mikobautista/tartantrades/logging"
+)
+
+type ChannelVar struct {
+	v                 interface{}
+	getChannel        chan getRequest
+	setChannel        chan setRequest
+	manipulateChannel chan manipulateRequest
+}
+
+type getRequest struct {
+	r chan interface{}
+}
+
+type setRequest struct {
+	v interface{}
+	r chan interface{}
+}
+
+type manipulateRequest struct {
+	f func(interface{}) interface{}
+	r chan interface{}
+}
+
+var LOG = logging.NewLogger(true)
+
+func NewChannelVar() ChannelVar {
+	rv := ChannelVar{
+		v:                 make(map[interface{}]interface{}),
+		getChannel:        make(chan getRequest, 200),
+		setChannel:        make(chan setRequest, 200),
+		manipulateChannel: make(chan manipulateRequest, 200),
+	}
+	go rv.listen()
+	return rv
+}
+
+func (tm *ChannelVar) Get() interface{} {
+	r := make(chan interface{})
+	tm.getChannel <- getRequest{
+		r: r,
+	}
+	select {
+	case v := <-r:
+		return v
+	}
+}
+
+func (tm *ChannelVar) Set(v interface{}) {
+	r := make(chan interface{})
+	tm.setChannel <- setRequest{
+		v: v,
+		r: r,
+	}
+	select {
+	case _ = <-r:
+		return
+	}
+}
+
+func (tm *ChannelVar) ManipulateValue(f func(interface{}) interface{}) {
+	r := make(chan interface{})
+	tm.manipulateChannel <- manipulateRequest{
+		f: f,
+		r: r,
+	}
+	select {
+	case _ = <-r:
+		return
+	}
+}
+
+func (tm *ChannelVar) listen() {
+	for {
+		select {
+		case c := <-tm.getChannel:
+			c.r <- tm.v
+		case c := <-tm.setChannel:
+			tm.v = c.v
+			c.r <- struct{}{}
+		case c := <-tm.manipulateChannel:
+			tm.v = c.f(tm.v)
+			c.r <- struct{}{}
+		}
+	}
+}
--- src/github.com/mikobautista/tartantrades/rpc/resolver_rpc/rpc.go
+++ src/github.com/mikobautista/tartantrades/rpc/resolver_rpc/rpc.go
@@ -1,14 +1,8 @@
 package resolver_rpc
 
-// STAFF USE ONLY! Students should not use this interface in their code.
 type TradeResolver interface {
 	RegisterServer(*RegisterArgs, *RegisterReply) error
 	GetServers(*GetServersArgs, *GetServersReply) error
-	Get(*GetArgs, *GetReply) error
-	GetList(*GetArgs, *GetListReply) error
-	Put(*PutArgs, *PutReply) error
-	AppendToList(*PutArgs, *PutReply) error
-	RemoveFromList(*PutArgs, *PutReply) error
 }
 
 type Resolver struct {
