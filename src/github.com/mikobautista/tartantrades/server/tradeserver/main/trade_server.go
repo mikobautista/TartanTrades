fa90082e2af732380ecb5cb817dc66a8b052d331
Mat Gray <me@mgray.me>
2014-04-25 03:41:57 -0400

    Created TradeSever type, added methods to type

--- src/github.com/mikobautista/tartantrades/server/tradeserver/main/trade_server.go
+++ src/github.com/mikobautista/tartantrades/server/tradeserver/main/trade_server.go
@@ -16,6 +16,20 @@ import (
 	_ "github.com/ziutek/mymysql/godrv"
 )
 
+type TradeServer struct {
+	db               *sql.DB
+	resolverhostport string
+	tcpPort          int
+	httpPort         int
+	tableName        string
+	dbUser           string
+	dbPw             string
+	thisHostPort     string
+	index            channelvar.ChannelVar
+	lowerBound       channelvar.ChannelVar
+	tradeServers     channelslice.ChannelSlice
+}
+
 const (
 	CONN_HOST = "localhost"
 	CONN_TYPE = "tcp"
@@ -24,8 +38,79 @@ const (
 var (
 	LOG          = logging.NewLogger(true)
 	index        = channelvar.NewChannelVar()
+	lowerBound   = channelvar.NewChannelVar()
 	tradeServers = channelslice.NewChannelSlice()
 )
+
+func NewTradeSever(
+	resolverhostport string,
+	tcpPort int,
+	httpPort int,
+	tableName string,
+	dbUser string,
+	dbPw string) *TradeServer {
+
+	return &TradeServer{
+		resolverhostport: resolverhostport,
+		tcpPort:          tcpPort,
+		httpPort:         httpPort,
+		tableName:        tableName,
+		dbUser:           dbUser,
+		dbPw:             dbPw,
+		thisHostPort:     fmt.Sprintf("%s:%d", CONN_HOST, tcpPort),
+		index:            channelvar.NewChannelVar(),
+		lowerBound:       channelvar.NewChannelVar(),
+		tradeServers:     channelslice.NewChannelSlice(),
+	}
+}
+
+func (ts *TradeServer) Start() {
+	lowerBound.Set(0)
+
+	LOG.LogVerbose("Establishing Connection to Database...")
+	db, err := sql.Open("mymysql", fmt.Sprintf("%s/%s/%s", ts.tableName, ts.dbUser, ts.dbPw))
+	ts.db = db
+	LOG.CheckForError(err, true)
+	defer db.Close()
+
+	// Launch HTTP server listening for clients
+	m := make(map[string]shared.HandlerType)
+	m["/availableblocks/"] = httpGetAvailableBlocks
+	m["/sell/"] = httpGetAvailableBlocks
+	LOG.LogVerbose("Starting HTTP server on port %d", ts.httpPort)
+	go shared.NewHttpServer(ts.httpPort, m)
+
+	l, err := net.Listen(CONN_TYPE, fmt.Sprintf("%s:%d", CONN_HOST, ts.tcpPort))
+	LOG.CheckForError(err, true)
+	// Close the listener when the application closes.
+	defer l.Close()
+	LOG.LogVerbose("Trade server TCP server started on %s", ts.thisHostPort)
+
+	// (miko) Connect to the resolver via TCP
+	LOG.LogVerbose("Connecting to resolver (%s)...", ts.resolverhostport)
+	conn, err := net.Dial("tcp", ts.resolverhostport)
+	LOG.CheckForError(err, true)
+
+	connectMessage := shared.ResolverMessage{
+		Type:    shared.CONNECT,
+		Payload: ts.thisHostPort,
+		Id:      uint32(ts.httpPort),
+	}
+	marshalledMessage, _ := json.Marshal(connectMessage)
+	conn.Write(marshalledMessage)
+
+	ts.listenToResolver(conn, func(id uint32) {
+		for {
+			// Listen for an incoming connection.
+			conn, err := l.Accept()
+			LOG.CheckForError(err, false)
+
+			// Handle connections in a new goroutine.
+			go ts.onTradeServerConnection(conn)
+		}
+	})
+
+}
 
 func main() {
 	var resolverhostport = flag.String("resolverhostport", "127.0.0.1:1234", "Resolver hostport")
@@ -35,52 +120,18 @@ func main() {
 	var dbUser = flag.String("db_user", "trader", "Database username")
 	var dbPw = flag.String("db_pw", "password", "Database password")
 	flag.Parse()
+	svr := NewTradeSever(
+		*resolverhostport,
+		*tcpPort,
+		*httpPort,
+		*tableName,
+		*dbUser,
+		*dbPw)
 
-	LOG.LogVerbose("Establishing Connection to Database...")
-	db, err := sql.Open("mymysql", fmt.Sprintf("%s/%s/%s", *tableName, *dbUser, *dbPw))
-	LOG.CheckForError(err, true)
-	defer db.Close()
-
-	// Launch HTTP server listening for clients
-	m := make(map[string]shared.HandlerType)
-	m["/availableblocks/"] = httpGetAvailableBlocks
-	m["/sell/"] = httpGetAvailableBlocks
-	LOG.LogVerbose("Starting HTTP server on port %d", *httpPort)
-	go shared.NewHttpServer(*httpPort, m)
-
-	l, err := net.Listen(CONN_TYPE, fmt.Sprintf("%s:%d", CONN_HOST, *tcpPort))
-	LOG.CheckForError(err, true)
-	// Close the listener when the application closes.
-	defer l.Close()
-	thisHostPort := fmt.Sprintf("%s:%d", CONN_HOST, *tcpPort)
-	LOG.LogVerbose("Trade server TCP server started on %s", thisHostPort)
-
-	// (miko) Connect to the resolver via TCP
-	LOG.LogVerbose("Connecting to resolver (%s)...", *resolverhostport)
-	conn, err := net.Dial("tcp", *resolverhostport)
-	LOG.CheckForError(err, true)
-
-	connectMessage := shared.ResolverMessage{
-		Type:    shared.CONNECT,
-		Payload: thisHostPort,
-		Id:      uint32(*httpPort),
-	}
-	marshalledMessage, _ := json.Marshal(connectMessage)
-	conn.Write(marshalledMessage)
-
-	listenToResolver(conn, thisHostPort, db, func(id uint32) {
-		for {
-			// Listen for an incoming connection.
-			conn, err := l.Accept()
-			LOG.CheckForError(err, false)
-
-			// Handle connections in a new goroutine.
-			go onTradeServerConnection(conn, db)
-		}
-	})
+	svr.Start()
 }
 
-func listenToResolver(conn net.Conn, thisHostPort string, db *sql.DB, callback func(uint32)) {
+func (ts *TradeServer) listenToResolver(conn net.Conn, callback func(uint32)) {
 	buf := make([]byte, 1024)
 	hasIdAssigned := false
 	var m shared.ResolverMessage
@@ -102,11 +153,11 @@ func listenToResolver(conn net.Conn, thisHostPort string, db *sql.DB, callback f
 			LOG.CheckForError(err, false)
 			connectMessage := shared.TradeMessage{
 				Type:    shared.WELCOME,
-				Payload: thisHostPort,
+				Payload: ts.thisHostPort,
 			}
 			marshalledMessage, _ := json.Marshal(connectMessage)
 			conn.Write(marshalledMessage)
-			go listenToTradeServer(conn, m.Payload, db)
+			go ts.listenToTradeServer(conn, m.Payload)
 
 		case shared.TRADE_SERVER_DROP:
 			LOG.LogVerbose("Dropping trade server: %s", m.Payload)
@@ -115,7 +166,7 @@ func listenToResolver(conn net.Conn, thisHostPort string, db *sql.DB, callback f
 	}
 }
 
-func onTradeServerConnection(conn net.Conn, db *sql.DB) {
+func (ts *TradeServer) onTradeServerConnection(conn net.Conn) {
 	buf := make([]byte, 1024)
 	var m shared.TradeMessage
 	n, err := conn.Read(buf)
@@ -124,12 +175,12 @@ func onTradeServerConnection(conn net.Conn, db *sql.DB) {
 	switch m.Type {
 	case shared.WELCOME:
 		LOG.LogVerbose("Recieved Welcome from %s", m.Payload)
-		go listenToTradeServer(conn, m.Payload, db)
+		go ts.listenToTradeServer(conn, m.Payload)
 	}
 
 }
 
-func listenToTradeServer(conn net.Conn, otherHostPort string, db *sql.DB) {
+func (ts *TradeServer) listenToTradeServer(conn net.Conn, otherHostPort string) {
 	for {
 		buf := make([]byte, 1024)
 		var m shared.TradeMessage
