809a9afc39e0b0cf264f8dc3d225132a10a0b2ea
Mat Gray <me@mgray.me>
2014-04-25 03:13:48 -0400

    create ResolverServer type and defined methods on this type

--- src/github.com/mikobautista/tartantrades/server/resolver/main/resolver.go
+++ src/github.com/mikobautista/tartantrades/server/resolver/main/resolver.go
@@ -13,7 +13,6 @@ import (
 
 	"github.com/mikobautista/tartantrades/channelmap"
 	"github.com/mikobautista/tartantrades/channelslice"
-	"github.com/mikobautista/tartantrades/channelvar"
 	"github.com/mikobautista/tartantrades/logging"
 	"github.com/mikobautista/tartantrades/server/shared"
 
@@ -26,10 +25,7 @@ const (
 )
 
 var (
-	LOG           = logging.NewLogger(true)
-	connectionMap = channelmap.NewChannelMap()
-	index         = channelvar.NewChannelVar()
-	tradeServers  = channelslice.NewChannelSlice()
+	LOG = logging.NewLogger(true)
 )
 
 type user struct {
@@ -44,6 +40,73 @@ type Token struct {
 	Experation time.Time
 }
 
+type ResolverServer struct {
+	db              *sql.DB
+	connectionMap   channelmap.ChannelMap
+	tradeServers    channelslice.ChannelSlice
+	tcpPort         int
+	httpPort        int
+	tableName       string
+	dbUser          string
+	dbPw            string
+	sessionDuration int
+	checkExpires    bool
+}
+
+func NewResolverServer(
+	httpPort int,
+	tcpPort int,
+	tableName string,
+	dbUser string,
+	dbPw string,
+	sessionDuration int,
+	checkExpires bool) *ResolverServer {
+
+	return &ResolverServer{
+		connectionMap:   channelmap.NewChannelMap(),
+		tradeServers:    channelslice.NewChannelSlice(),
+		tcpPort:         tcpPort,
+		httpPort:        httpPort,
+		tableName:       tableName,
+		dbUser:          dbUser,
+		dbPw:            dbPw,
+		sessionDuration: sessionDuration,
+		checkExpires:    checkExpires,
+	}
+}
+
+func (rs *ResolverServer) start() {
+	LOG.LogVerbose("Establishing Connection to Database...")
+	db, err := sql.Open("mymysql", fmt.Sprintf("%s/%s/%s", rs.tableName, rs.dbUser, rs.dbPw))
+	rs.db = db
+	LOG.CheckForError(err, true)
+	defer db.Close()
+
+	m := make(map[string]shared.HandlerType)
+	m["/servers/"] = httpGetTradeServerHandler(rs.tradeServers)
+	m["/login/"] = httpLoginHandler(rs.db, rs.sessionDuration)
+	m["/validate/"] = httpAuthenticateHandler(rs.db, rs.checkExpires)
+
+	LOG.LogVerbose("Resolver HTTP server starting on %s:%d", CONN_HOST, rs.httpPort)
+	go shared.NewHttpServer(rs.httpPort, m)
+
+	// Listen for incoming connections.
+	l, err := net.Listen(CONN_TYPE, fmt.Sprintf("%s:%d", CONN_HOST, rs.tcpPort))
+	LOG.CheckForError(err, true)
+	// Close the listener when the application closes.
+	defer l.Close()
+	LOG.LogVerbose("Resolver TCP server started on %s:%d", CONN_HOST, rs.tcpPort)
+
+	for {
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		LOG.CheckForError(err, false)
+		// Handle connections in a new goroutine.
+		go rs.onTradeServerConnection(conn)
+	}
+
+}
+
 func main() {
 	var tcpPort = flag.Int("tradeport", 1234, "Port to start resolver trade server on")
 	var httpPort = flag.Int("httpport", 80, "Resolver http port")
@@ -51,40 +114,24 @@ func main() {
 	var dbUser = flag.String("db_user", "resolver", "Database username")
 	var dbPw = flag.String("db_pw", "password", "Database password")
 	var sessionDuration = flag.Int("session_duration", 30, "Duration of a login session in minutes")
-	var sessionExperation = flag.Bool("sessionExperation", true, "Sessions expire")
+	var sessionExperation = flag.Bool("checkSessionExperation", true, "Sessions expire")
 	flag.Parse()
 
-	LOG.LogVerbose("Establishing Connection to Database...")
-	db, err := sql.Open("mymysql", fmt.Sprintf("%s/%s/%s", *tableName, *dbUser, *dbPw))
-	LOG.CheckForError(err, true)
-	defer db.Close()
+	svr := NewResolverServer(
+		*httpPort,
+		*tcpPort,
+		*tableName,
+		*dbUser,
+		*dbPw,
+		*sessionDuration,
+		*sessionExperation)
 
-	m := make(map[string]shared.HandlerType)
-	m["/servers/"] = httpGetTradeServerHandler
-	m["/login/"] = httpLoginHandler(db, *sessionDuration)
-	m["/validate/"] = httpAuthenticateHandler(db, *sessionExperation)
+	svr.start()
 
-	LOG.LogVerbose("Resolver HTTP server starting on %s:%d", CONN_HOST, *httpPort)
-	go shared.NewHttpServer(*httpPort, m)
-
-	// Listen for incoming connections.
-	l, err := net.Listen(CONN_TYPE, fmt.Sprintf("%s:%d", CONN_HOST, *tcpPort))
-	LOG.CheckForError(err, true)
-	// Close the listener when the application closes.
-	defer l.Close()
-	LOG.LogVerbose("Resolver TCP server started on %s:%d", CONN_HOST, *tcpPort)
-
-	for {
-		// Listen for an incoming connection.
-		conn, err := l.Accept()
-		LOG.CheckForError(err, false)
-		// Handle connections in a new goroutine.
-		go onTradeServerConnection(conn, db, *sessionExperation)
-	}
 }
 
 // Handles incoming requests.
-func onTradeServerConnection(conn net.Conn, db *sql.DB, sessionExperation bool) {
+func (rs *ResolverServer) onTradeServerConnection(conn net.Conn) {
 	// Make a buffer to hold incoming data.
 	buf := make([]byte, 1024)
 	var m shared.ResolverMessage
@@ -92,8 +139,6 @@ func onTradeServerConnection(conn net.Conn, db *sql.DB, sessionExperation bool) 
 	n, err := conn.Read(buf)
 	LOG.CheckForError(err, false)
 	_ = json.Unmarshal(buf[:n], &m)
-	LOG.LogVerbose("%s", string(buf))
-	LOG.LogVerbose("%d - %s", int(m.Type), m.Payload)
 
 	connectionHostPort := m.Payload
 	h := GetHash(connectionHostPort)
@@ -109,7 +154,7 @@ func onTradeServerConnection(conn net.Conn, db *sql.DB, sessionExperation bool) 
 	conn.Write(mId)
 
 	// Notify trade servers of new server joining
-	for _, v := range connectionMap.Raw() {
+	for _, v := range rs.connectionMap.Raw() {
 		joinMessage := shared.ResolverMessage{
 			Type:    shared.TRADE_SERVER_JOIN,
 			Payload: connectionHostPort,
@@ -118,11 +163,11 @@ func onTradeServerConnection(conn net.Conn, db *sql.DB, sessionExperation bool) 
 		v.(net.Conn).Write(marshalledMessage)
 	}
 
-	connectionMap.Put(h, conn)
-	tradeServers.Append(connectionHostPort)
+	rs.connectionMap.Put(h, conn)
+	rs.tradeServers.Append(connectionHostPort)
 
 	// Send a hash back to person contacting us.
-	go listenToTradeServer(conn, h, connectionHostPort, db, sessionExperation)
+	go rs.listenToTradeServer(conn, h, connectionHostPort)
 }
 
 func GetHash(key string) uint32 {
@@ -131,7 +176,7 @@ func GetHash(key string) uint32 {
 	return hasher.Sum32()
 }
 
-func listenToTradeServer(conn net.Conn, id uint32, connectionHostPort string, db *sql.DB, checkExpires bool) {
+func (rs *ResolverServer) listenToTradeServer(conn net.Conn, id uint32, connectionHostPort string) {
 	buf := make([]byte, 1024)
 
 	for {
@@ -139,11 +184,11 @@ func listenToTradeServer(conn net.Conn, id uint32, connectionHostPort string, db
 		if err != nil {
 			LOG.LogVerbose("TCP error for trade server id %d (%s), dropping...", id, connectionHostPort)
 			conn.Close()
-			connectionMap.Rem(id)
-			tradeServers.Rem(connectionHostPort)
+			rs.connectionMap.Rem(id)
+			rs.tradeServers.Rem(connectionHostPort)
 
 			//Notify Clients of drop
-			for _, v := range connectionMap.Raw() {
+			for _, v := range rs.connectionMap.Raw() {
 				joinMessage := shared.ResolverMessage{
 					Type:    shared.TRADE_SERVER_DROP,
 					Payload: connectionHostPort,
@@ -154,17 +199,17 @@ func listenToTradeServer(conn net.Conn, id uint32, connectionHostPort string, db
 
 			return
 		}
-		handleMessageFromTradeServer(buf[:n], id, conn, db, checkExpires)
+		rs.handleMessageFromTradeServer(buf[:n], id, conn)
 	}
 }
 
-func handleMessageFromTradeServer(message []byte, id uint32, conn net.Conn, db *sql.DB, checkExpires bool) {
+func (rs *ResolverServer) handleMessageFromTradeServer(message []byte, id uint32, conn net.Conn) {
 	var m shared.ResolverMessage
 	_ = json.Unmarshal(message, &m)
 
 	switch m.Type {
 	case shared.AUTHENTICATION:
-		conn.Write([]byte(fmt.Sprintf("%d", tokenToUserId(m.Payload, db, checkExpires))))
+		conn.Write([]byte(fmt.Sprintf("%d", tokenToUserId(m.Payload, rs.db, rs.checkExpires))))
 	}
 }
 
@@ -172,10 +217,12 @@ func handleMessageFromTradeServer(message []byte, id uint32, conn net.Conn, db *
 //                  HTTP handlers
 // ----------------------------------------------------
 
-func httpGetTradeServerHandler(w http.ResponseWriter, r *http.Request) {
-	// Print out all of the tradeservers
-	for _, hostport := range tradeServers.GetStringList() {
-		fmt.Fprintf(w, "%s\n", hostport)
+func httpGetTradeServerHandler(tradeServers channelslice.ChannelSlice) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Print out all of the tradeservers
+		for _, hostport := range tradeServers.GetStringList() {
+			fmt.Fprintf(w, "%s\n", hostport)
+		}
 	}
 }
 
